d1540c9bd2c75d04d251b252d2978a3961835e54
noraalsuwaidi <143498128+noraalsuwaidi@users.noreply.github.com>
2023-09-18 13:09:23 +0000

    Updated account

--- account.cpp
+++ account.cpp
@@ -1,19 +1,6 @@
-// Create a C++ class for a bank account following the given specifications.
-// You need to complete the 'Account' class in this 'account.h' file.
-// The 'Account' class should include the following:
-// 1. A private member variable 'balance' of type double to store the account balance.
-// 2. Public constructors:
-//    a. A default constructor that initializes 'balance' to 0.
-//    b. A parameterized constructor that takes an initial balance as an argument.
-// 3. Public member functions:
-//    a. Getter function 'getBalance' to return the current balance.
-//    b. Setter function 'setBalance' to set the balance to a new value.
-//    c. Function 'deposit' that takes an amount and adds it to the balance.
-//    d. Function 'withdraw' that takes an amount and subtracts it from the balance.
-// 4. A destructor that displays a message like "Account with balance <balance_value> is being destroyed."
-
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
+#include <iostream>
 
 class Account {
 private:
@@ -23,58 +10,69 @@ private:
 public:
     // Default constructor.
     // Initialize 'balance' to 0.
-    // TODO: Implement the default constructor.
-    Account(){
-        double balance=0;
+    Account() {
+        balance = 0;
     }
 
     // Parameterized constructor.
     // Initialize 'balance' with the provided 'initialBalance'.
-    // TODO: Implement the parameterized constructor.
-    Account(double initalBalance){
-        double balance= initalBalance;  
+    Account(double initialBalance) {
+        balance = initialBalance;
     }
 
     // Getter function to return the current balance.
-    // TODO: Implement the 'getBalance' function.
-    double getBalance(){
+    double getBalance() {
         return balance;
     }
 
-
     // Setter function to set the balance to a new value.
-    // TODO: Implement the 'setBalance' function.
-    void setBalance(double newBalance){
-        balance= newBalance;
+    void setBalance(double newBalance) {
+        balance = newBalance;
     }
 
     // Function to deposit an amount into the account.
-    // TODO: Implement the 'deposit' function.
-    void deposit(double amount){
+    void deposit(double amount) {
         balance += amount;
-
     }
 
     // Function to withdraw an amount from the account.
-    // TODO: Implement the 'withdraw' function.
-     void withdraw(double amount);
-      if (amount <= balance)
-      {
-        balance-= amount;
-      }
-       
-     else {
-        std::cout<<"Insufficient funds for withdrawal"<<"\n";
-     }
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            std::cout << "Insufficient funds !" << "\n";
+        }
+    }
 
     // Destructor.
     // Display a message indicating the account is being destroyed.
-    // TODO: Implement the destructor.
-    ~Account(){
-    std::cout << "Account with balance" << balance << "\n";
+    ~Account() {
+        std::cout << "Account with balance " << balance << " is being destroyed." << "\n";
     }
-    
 };
 
+int main() {
+    Account account1;  // Default constructor, balance = 0
+    Account account2(1000);  // Parameterized constructor, balance = 1000
+
+    std::cout << "Account 1 balance: " << account1.getBalance() << std::endl;
+    std::cout << "Account 2 balance: " << account2.getBalance() << std::endl;
+
+    account1.deposit(500);
+    account2.withdraw(200);
+
+    std::cout << "Account 1 balance after deposit: " << account1.getBalance() << std::endl;
+    std::cout << "Account 2 balance after withdrawal: " << account2.getBalance() << std::endl;
+
+    account1.setBalance(3000);
+
+    // Attempt to withdraw more than the balance
+    account1.withdraw(4000);
+
+    return 0;
 
 #endif // ACCOUNT_H
+
+
+
+}
